d557a93ccfeb1409253f5ab7fc48ec2970327298
lex <ade77amos@gmail.com>
2024-12-27 11:44:47 -0600

    Add app description, image and certs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-col1,col2 = st.columns(2)
+col1,col5, col2= st.columns(3)
 
 
 with col1:
@@ -17,15 +17,26 @@ I excel at leveraging industry-leading tools like Kubernetes for container orche
 Additionally, I am aspiring to become a Full Stack Python Developer, aiming to expand my technical expertise and contribute to building robust end-to-end solutions."""
     st.write(content)
 
+with col5:
+    st.image("images/aws.jpg", width=100)
+    st.image("images/dynatrace.png", width=100)
+    st.image("images/k8.png", width=100)
+
 st.text("Below you can find some of the apps i have built in python . Feel free to contact me!")
 
-col3, col4 = st.columns(2)
+col3, empty_col,  col4 = st.columns([1.5,0.5,1.5])
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source Code]({row['url']})")
