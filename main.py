60bd144764acf777c657950e9d481947eb2771b1
lex <ade77amos@gmail.com>
2024-12-27 11:22:36 -0600

    Render the app titles into columns

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
 
 with col1:
-    st.image("images/Lekan.jpg", width=300)
+    st.image("images/Lekan.jpg", width=300 )
 
 with col2:
     st.title("Olalekan Amos Adewopo")
@@ -17,3 +18,14 @@ Additionally, I am aspiring to become a Full Stack Python Developer, aiming to e
     st.write(content)
 
 st.text("Below you can find some of the apps i have built in python . Feel free to contact me!")
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
